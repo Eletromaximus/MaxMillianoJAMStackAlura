5bc95940ff3dd37ff1aab8feb70c893ee229d0fd
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-02-23 14:50:18 -0300

    chore(deploay test again): deploay test again

--- docs/static/qWViyfuzGB6WcQ3w7U7AQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-5736928cf07272a28df6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bcf60a55c9be73cb7a88.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
