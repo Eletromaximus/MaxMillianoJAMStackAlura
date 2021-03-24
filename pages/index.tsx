133faf5706218a3e9912de1de66e615579db934f
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-03-24 14:27:20 -0300

    fix(upgrade libs): upgrade libs

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ function HomeScreen () {
             description="Relógio pomodoro com React e typescript"
             title="Relógio Pomodoro"
             url="https://raw.githubusercontent.com/Eletromaximus/PomodoroClock/master/pomodoro.png"
-            endereco="https://github.com/Eletromaximus/PomodoroClock"
+            endereco="/project/PomodoroClock"
           />
         </Grid.Row>
 
@@ -30,19 +30,19 @@ function HomeScreen () {
             url="https://github.com/Eletromaximus/semanaomnistack11/blob/master/x2.png?raw=true"
             title="Semana Omnistack 11"
             alt="semana omnistack 11"
-            href="https://github.com/Eletromaximus/semanaomnistack11"
+            href="/project/semanaomnistack11"
           />
           <Card
             url="https://github.com/Eletromaximus/NLW3/blob/master/x.png?raw=true"
             title="Next Level Week 3"
             alt="next level week 3"
-            href="https://github.com/Eletromaximus/NLW3"
+            href="/project/NLW3"
           />
           <Card
             url="https://github.com/Eletromaximus/InstaAlura/raw/main/instaAlura.png"
             title="InstaAlura"
             alt="instaAlura"
-            href="https://github.com/Eletromaximus/InstaAlura"
+            href="/project/InstaAlura"
           />
         </Grid.Row>
       </WrapperProjects>
