af87fadd5552cbad2ca655ffe00703909de0cd54
Max Milliano <maxmillianox@protonmail.com>
2022-03-18 10:50:43 -0300

    fix(imagens corrigidas e atualizadas): imagens corrgidas e document adicionado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
