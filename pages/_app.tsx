b020826b06d361a13408b4eadfe47db465a6e67e
Max Milliano <maxmillianox@protonmail.com>
2022-03-17 22:03:22 -0300

    fix(corrigindo links com estilo e atualizaçao): novas atualizaçoes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,24 +3,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
-          rel='stylesheet'
-        />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
-        <link href='https://fonts.googleapis.com/css2?family=Poppins&display=swap' rel='stylesheet'/>
-        <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
-
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      </Head>
-        <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
